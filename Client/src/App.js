d48cb7b104807fff7619de595baae9320963908a
navinAce <navinraj99305@gmail.com>
2024-03-06 00:13:49 +0530

    Add loading state and verification status to App component

--- Client/src/App.js
+++ Client/src/App.js
@@ -12,7 +12,9 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isVerify, setIsVerify] = useState(false);
   const [isLoading, setIsLoading] = useState(true); // Add loading state
+  const [isLoading1, setIsLoading1] = useState(true);
 
   // Get the current pathname
   const pathname = window.location.pathname;
@@ -33,13 +35,41 @@ function App() {
     checkStatus();
   }, []);
 
+  useEffect(() => {
+    const VerifyStatus = async () => {
+      try {
+        const response = await fetch('/api/v1/users/checkstatus');
+        const data = await response.json();
+        if (data.data === true) {
+          setIsVerify(true);
+        }else{
+          setIsVerify(false);
+        }
+      } catch (error) {
+        setIsVerify(false);
+      }
+      setIsLoading1(false); // Update loading state once the check is completed
+    };
+    VerifyStatus();
+  }, []);
+
+
   const privateRoutes = () => {
     let auth = { 'token': isAuthenticated };
     return (auth.token ? <Outlet /> : <Navigate to="/api/v1/users/login" />);
   }
 
+  const verifyRoutes = () => {
+    let auth = { 'token': isVerify };
+    return (auth.token ? <Outlet /> : <Navigate to="/api/v1/users/login" />);
+  }
+
   // Render loading state while checking authentication status
   if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isLoading1) {
     return <div>Loading...</div>;
   }
 
@@ -56,9 +86,12 @@ function App() {
             <Route path="/api/v1/users/contact" element={<Contact />} />
             <Route path="/api/v1/users/editprofile" element={<EditProf />} />
             <Route element={privateRoutes()}>
-              <Route path="/api/v1/users/bikedetails" element={<List />} />
               <Route path="/api/v1/users/dashboard" element={<Dashboard />} />
             </Route>
+            <Route element={verifyRoutes()}>
+              <Route path="/api/v1/users/bikedetails" element={<List />} /> 
+            </Route>
+            
           </Routes>
         </div>
         <Footer />
